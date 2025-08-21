285f17441fc7cfc28f5c074998950ee5b3159c78
Roman Martynov <kseexy@mail.ru>
2025-08-21 18:59:49 +0300

    fix unused import

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import "./style.css";
 import { defineComponents } from "./defineComponents";
-import "@tinymce/tinymce-webcomponent";
 import { setupEditor } from "./setupEditor";
 
 defineComponents();
